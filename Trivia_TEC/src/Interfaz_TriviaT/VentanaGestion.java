da63539f7505f4badd45fc9833dea829155d2208
Musiktt <musiktt@gmail.com>
2014-10-10 07:37:11 -0600

    ventanaspreguntashechas

--- Trivia_TEC/src/Interfaz_TriviaT/VentanaGestion.java
+++ Trivia_TEC/src/Interfaz_TriviaT/VentanaGestion.java
@@ -58,6 +58,8 @@ public class VentanaGestion extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         formulaP = new javax.swing.JButton();
         jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        comboTipoP = new javax.swing.JComboBox();
         jPanel3 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
 
@@ -221,30 +223,49 @@ public class VentanaGestion extends javax.swing.JFrame {
             }
         });
 
+        jLabel9.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jLabel9.setText("Sección de preguntas del juego");
+
+        jLabel10.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel10.setText("Elije el tipo de preguntar a formular");
+
+        comboTipoP.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Falso o verdadero", "Selección única", "Selección múltiple" }));
+        comboTipoP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comboTipoPActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(173, 173, 173)
+                        .addGap(150, 150, 150)
                         .addComponent(jLabel9))
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(216, 216, 216)
-                        .addComponent(formulaP)))
-                .addContainerGap(210, Short.MAX_VALUE))
+                        .addComponent(jLabel10)
+                        .addGap(42, 42, 42)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(formulaP)
+                            .addComponent(comboTipoP, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(281, 281, 281))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addGap(26, 26, 26)
                 .addComponent(jLabel9)
-                .addGap(18, 18, 18)
+                .addGap(38, 38, 38)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel10)
+                    .addComponent(comboTipoP, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(68, 68, 68)
                 .addComponent(formulaP)
-                .addContainerGap(242, Short.MAX_VALUE))
+                .addContainerGap(144, Short.MAX_VALUE))
         );
 
         jTabbedPane2.addTab("Preguntas", jPanel2);
@@ -253,11 +274,11 @@ public class VentanaGestion extends javax.swing.JFrame {
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 533, Short.MAX_VALUE)
+            .addGap(0, 774, Short.MAX_VALUE)
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 323, Short.MAX_VALUE)
+            .addGap(0, 348, Short.MAX_VALUE)
         );
 
         jTabbedPane2.addTab("Curso", jPanel3);
@@ -365,13 +386,32 @@ public class VentanaGestion extends javax.swing.JFrame {
     }//GEN-LAST:event_bExaminarActionPerformed
 
     private void formulaPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_formulaPActionPerformed
-        // TODO add your handling code here:
-        PreguntaFV nuevas = new PreguntaFV();
-        this.hide();
-        nuevas.show();
         
+
+    //Se define una variable para obtener el tipo de pregunta seleccionado
+        int tipoPS = comboTipoP.getSelectedIndex();
+        
+        if (tipoPS==0){//Se establece reglas condicionales para lograr abrir una ventana en especial
+            PreguntaFV nuevaFV = new PreguntaFV();
+            this.hide();
+            nuevaFV.show();
+        }
+        else if (tipoPS==1){
+            PreguntaSU nuevaSU = new PreguntaSU();
+            this.hide();
+            nuevaSU.show();    
+        }
+        else{
+            PreguntaSM nuevaSM = new PreguntaSM();
+            this.hide();
+            nuevaSM.show();
+        }
         
     }//GEN-LAST:event_formulaPActionPerformed
+
+    private void comboTipoPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboTipoPActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_comboTipoPActionPerformed
 
     /**
      * @param args the command line arguments
@@ -413,9 +453,11 @@ public class VentanaGestion extends javax.swing.JFrame {
     private javax.swing.JButton bExaminar;
     private javax.swing.JButton bRegistrar;
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JComboBox comboTipoP;
     private javax.swing.JButton formulaP;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
